11724f391019696844275af96ff65d021df995cf
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-02-28 16:59:53 +0100

    Update UserRepository.java
    Add a comment

--- src/recipes/persistence/UserRepository.java
+++ src/recipes/persistence/UserRepository.java
@@ -2,8 +2,10 @@ package recipes.persistence;
 
 import org.springframework.data.repository.CrudRepository;
 import recipes.model.User;
-
 import java.util.Optional;
+/**
+ * Interface is used to interact with the user table in the database.
+ */
 public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByEmailIgnoreCase(String email);
     User findByEmailIgnoreCase(String email);
