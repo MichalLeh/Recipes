d5f7093a122f297afba79786077f0fcde5185d32
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-02-28 16:59:03 +0100

    Update RecipeRepository.java
    Add a comment

--- src/recipes/persistence/RecipeRepository.java
+++ src/recipes/persistence/RecipeRepository.java
@@ -3,9 +3,10 @@ package recipes.persistence;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import recipes.model.Recipe;
-
 import java.util.List;
-
+/**
+ * Interface is used to interact with the recipe table in the database.
+ */
 @Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
     List<Recipe> findByNameContainingIgnoreCaseOrderByDateDesc(String name);
