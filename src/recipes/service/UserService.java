642a93d2de75c0f30efe8c3371b0609262a5d734
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-02-28 17:21:03 +0100

    Update UserService.java
    Add comments

--- src/recipes/service/UserService.java
+++ src/recipes/service/UserService.java
@@ -11,12 +11,29 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+    /**
+     * Registration of a new user.
+     *
+     * @param user New user to be registered
+     */
     public void add(User user) {
         userRepository.save(user);
     }
+    /**
+     * Check an existence of the email in database
+     *
+     * @param email Email to be checked
+     * @return True if the email already exist, false otherwise
+     */
     public Boolean existsByEmailIgnoreCase(String email){
         return userRepository.existsByEmailIgnoreCase(email);
     }
+    /**
+     * Find user by the email
+     *
+     * @param email The email to be used to retrieve the user
+     * @return Return the user
+     */
     public User findByEmailIgnoreCase(String email){
         return userRepository.findByEmailIgnoreCase(email);
     }
