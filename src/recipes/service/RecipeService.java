8b786b4077f4bbacecc77fe251e8b6e1a2f94ad2
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-02-28 17:22:04 +0100

    Update RecipeService.java
    Add comments

--- src/recipes/service/RecipeService.java
+++ src/recipes/service/RecipeService.java
@@ -16,15 +16,38 @@ public class RecipeService {
     public RecipeService(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
     }
+    /**
+     * Find a recipe
+     *
+     * @param id Find a recipe by id
+     * @return Recipe if exists otherwise null
+     */
     public Optional<Recipe> findById(Long id) {
         return recipeRepository.findById(id);
     }
+    /**
+     * Add a new recipe
+     *
+     * @param recipe Object containing the recipe information
+     * @return The ID of the newly added recipe
+     */
     public long add(Recipe recipe) {
         return recipeRepository.save(recipe).getId();
     }
+    /**
+     * Delete the recipe
+     *
+     * @param id The ID of the recipe to delete
+     */
      public void deleteById(long id) {
         recipeRepository.deleteById(id);
     }
+    /**
+     * Update the recipe
+     *
+     * @param id The ID of the recipe to update
+     * @param recipe The recipe to update
+     */
     public void updateById(long id, Recipe recipe) {
         Optional<Recipe> optional = recipeRepository.findById(id);
         if (optional.isPresent()) {
@@ -33,9 +56,21 @@ public class RecipeService {
             recipeRepository.save(oldRecipe);
         }
     }
+    /**
+     * Find recipe(s)
+     *
+     * @param category The recipe(s) to be retrieved by a category
+     * @return List containing the recipe(s)
+     */
     public List<Recipe> findByCategory(String category) {
         return recipeRepository.findByCategoryIgnoreCaseOrderByDateDesc(category);
     }
+    /**
+     * Find recipe(s)
+     *
+     * @param name The recipe(s) to be retrieved by a name
+     * @return List containing the recipe(s)
+     */
     public List<Recipe> findByName(String name) {
         return recipeRepository.findByNameContainingIgnoreCaseOrderByDateDesc(name);
     }
