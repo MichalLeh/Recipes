a3b316cf1224fab7e1c58b70e961331c9d76dc06
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-02-28 17:18:03 +0100

    Update WebSecurity.java
    Add comments

--- src/recipes/security/WebSecurity.java
+++ src/recipes/security/WebSecurity.java
@@ -9,18 +9,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
+    
     public WebSecurity(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
+    /**
+    * Configure the {@link AuthenticationManagerBuilder} to specify which UserDetailsService and
+    * {@link PasswordEncoder} to use.
+    *
+    * @param auth The {@link AuthenticationManagerBuilder} to use
+    * @throws Exception If an error occurs
+    */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(getEncoder());
     }
+   /**
+    * Configure the security of the web application by restricting access based on the HttpServletRequest.
+    *
+    * @param http the {@link HttpSecurity} to modify
+    * @throws Exception if an error occurs
+    */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -38,9 +53,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .headers().frameOptions().disable();
     }
-    //Password encoder to be used in our database
+    /**
+     * Encrypt the password of the user using the BCryptPasswordEncoder.
+     *
+     * @return the {@link PasswordEncoder} to use
+     */
     @Bean
-    PasswordEncoder getEncoder(){
+    PasswordEncoder getEncoder() {
         return new BCryptPasswordEncoder(10);
     }
 }
