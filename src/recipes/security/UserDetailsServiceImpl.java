ed528b3589401340d9654f2e72ce328bc883120a
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2023-02-28 17:12:20 +0100

    Update UserDetailsServiceImpl.java
    Add comment

--- src/recipes/security/UserDetailsServiceImpl.java
+++ src/recipes/security/UserDetailsServiceImpl.java
@@ -4,9 +4,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
 import recipes.model.User;
 import recipes.persistence.UserRepository;
-
+/**
+ * This class implements the UserDetailsService interface and is used by Spring Security.
+ */
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
